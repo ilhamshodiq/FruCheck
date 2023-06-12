e0fb197d1dab1dcaae7cfc1c4986e00595cf9f43
Ilham Shodiq Mu'arif Billah <ilhambheh@gmail.com>
2023-06-13 00:09:05 +0700

    Update index.js
    nambah get detail buah
    tolong cek @yoshiinne64 @khailila

--- api/index.js
+++ api/index.js
@@ -103,6 +103,25 @@ app.get('/buah', (req, res) => {
     });
 });
 
+// Read specific buah detail
+// GET
+app.get("/buah/:id", (req, res) => {
+  try {
+    const reqDoc = db.collection("daftar_buah").doc(req.params.id);
+
+    reqDoc.get().then((buahDetail) => {
+      if (!buahDetail.exists) {
+        return res.status(404).send({message: 'Tidak ada buah yang ditemukan', error: true, message: "Buah tidak ditemukan" });
+      }
+
+      const response = buahDetail.data();
+      return res.status(200).send({message : 'Data buah berhasil diambil', error: false, data: response });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ status: "Failed", message: error });
+  }
+});
 
 // Start the application server
 const port = process.env.PORT || 8080;
