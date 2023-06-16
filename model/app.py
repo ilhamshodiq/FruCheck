5ce70312a4e472d88b4654b53f0130b465c8e582
yoshiinne64 <90911116+yoshiinne64@users.noreply.github.com>
2023-06-16 21:12:49 +0700

    Update app.py

--- model/app.py
+++ model/app.py
@@ -25,10 +25,7 @@ def upload_image():
     image_file = request.files['image']
     
     if image_file:
-        temp_image_path = 'temp_image.jpg'
-        image_file.save(temp_image_path)
-        
-        image = Image.open(temp_image_path)
+        image = Image.open(image_file)
         image = image.convert('RGB')
         image = image.resize((64, 64))
         image_array = np.array(image) / 255.0
@@ -54,9 +51,10 @@ def upload_image():
         
         bucket = storage_client.bucket(bucket_name)
         blob = bucket.blob(image_file.filename)
-        blob.upload_from_filename(temp_image_path)
-        
-        os.remove(temp_image_path)
+        blob.upload_from_string(
+            image_file.read(),
+            content_type=image_file.content_type
+        )
         
         label_predicted = predicted_label.replace('Segar', '').replace('Busuk', '')
         freshness = 'Segar' if 'Segar' in predicted_label else 'Busuk'
@@ -64,7 +62,7 @@ def upload_image():
         predicted_prob = np.max(predictions)
         accuracy = predicted_prob * 100
         
-        print('Image:', temp_image_path)
+        print('Image:', image_file.filename)
         print('Predicted Class:', label_predicted)
         print('Freshness:', freshness)
         print('Accuracy: {:.2f}%'.format(accuracy))
@@ -80,7 +78,7 @@ def upload_image():
         # Return the result as JSON response
         return jsonify(result), 200
 
-    return jsonify({'error':'No image uploaded!'}), 400
+    return jsonify({'No image uploaded!'}), 400
 
 if __name__== '__main__':
     app.run(debug=True)
